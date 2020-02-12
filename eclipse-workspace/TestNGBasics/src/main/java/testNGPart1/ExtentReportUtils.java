2887bb876ce319bac272bc8f9f49c4346d26ef60
Ankit <sunankiet@gmail.com>
2020-02-12 11:56:49 +0530

    updates

---
+++ eclipse-workspace/TestNGBasics/src/main/java/testNGPart1/ExtentReportUtils.java
@@ -0,0 +1,14 @@
+package testNGPart1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExtentReportUtils {
+	public WebDriver driver;
+	public ExtentReportUtils() {
+		
+		driver = new ChromeDriver();
+		
+	}
+
+}
